0ae7fd8cd4228fae39c8429bae9965141053c96e
itzKamran <siddiquekamran72@gmail.com>
2026-08-10 17:06:36 +0530

    Add switch case for day of the week
    Implement a switch case to log the day of the week based on a number.

---
+++ miniCalenderCase.js
@@ -0,0 +1,40 @@
+let num = 4
+
+  switch (num) {
+
+    case 1: 
+      console.log("Monday")
+      break
+    case 2:
+      console.log("Tuesday")
+      break
+    case 3:
+      console.log("Wednesday")
+      break
+    case 4:
+      console.log("Thursday")
+      break
+    case 5:
+      console.log("Friday")
+      break
+    case 6:
+      console.log("Saturday")
+      break
+    default:
+      console.log("Sunday")
+
+      
+// O/P : Thursday (print on browser console)
+
+
+
+
+
+
+
+
+
+
+
+
+  }
